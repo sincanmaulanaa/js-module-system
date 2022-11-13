f2b7b9bbf97f6181c76d036304dcc15fd9758470
Sincan Maulana <sincanmaulanaa@gmail.com>
2022-11-13 15:06:05 +0800

    configure scss and babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,29 @@ module.exports = {
   },
   watch: true,
   devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
